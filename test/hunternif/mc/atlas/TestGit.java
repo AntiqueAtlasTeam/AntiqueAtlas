cfe98a3733ce6081bb6d0be00e9ca0ea992be358
Hunternif <hunternif@gmail.com>
2013-10-24 22:13:18 +0400

    Implemented getting version from git in ant.

---
+++ test/hunternif/mc/atlas/TestGit.java
@@ -0,0 +1,29 @@
+package hunternif.mc.atlas;
+
+import java.io.File;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTag;
+
+public class TestGit {
+	public static void main(String[] args) {
+		try {
+			Git git = Git.open(new File("."));
+			List<Ref> tags = git.tagList().call();
+			Ref lastTag = tags.get(tags.size()-1);
+			Ref lastTagPeeled = git.getRepository().peel(lastTag);
+			ObjectId lastTagId = lastTag.getObjectId();
+			Iterable<RevCommit> log = git.log().call();
+			RevCommit lastCommit = log.iterator().next();
+			System.out.println(ObjectId.toString(lastTagPeeled.getPeeledObjectId()));
+			System.out.println(ObjectId.toString(lastCommit.getId()));
+			System.out.println("lol");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
