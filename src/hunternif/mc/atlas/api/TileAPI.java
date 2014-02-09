71355cc83dae32aec86719dd3d2da6218b28b98a
Hunternif <hunternif@gmail.com>
2014-02-09 05:01:53 +0300

    Updated comments on TileAPI

--- src/hunternif/mc/atlas/api/TileAPI.java
+++ src/hunternif/mc/atlas/api/TileAPI.java
@@ -31,9 +31,10 @@ public interface TileAPI {
 	 * automatically sent to all clients.
 	 * @param world		
 	 * @param dimension	dimension the chunk is located in.
-	 * @param tileName	the unique name you registered your tile with.
-	 * @param chunkX	x chunk coordinate. (block coordinate << 4)
-	 * @param chunkZ	z chunk coordinate. (block coordinate << 4)
+	 * @param tileName	the unique name you for your tile. You must use the
+	 * 			same name when setting textures for it.
+	 * @param chunkX	x chunk coordinate. (block coordinate >> 4)
+	 * @param chunkZ	z chunk coordinate. (block coordinate >> 4)
 	 */
 	void putCustomTile(World world, int dimension, String tileName, int chunkX, int chunkZ);
 }
