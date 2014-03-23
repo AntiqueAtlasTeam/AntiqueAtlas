70cb61e33b2d77ca5d8102967cfaf433eb50974a
Hunternif <hunternif@gmail.com>
2014-03-23 19:05:29 +0400

    Using Gradle

--- src/hunternif/mc/atlas/client/gui/ExportUpdateListener.java
+++
@@ -1,6 +0,0 @@
-package hunternif.mc.atlas.client.gui;
-
-public interface ExportUpdateListener {
-	void update(float percentage);
-	void setStatusString(String status);
-}
--- src/hunternif/mc/atlas/client/gui/ISelectListener.java
+++
@@ -1,9 +0,0 @@
-package hunternif.mc.atlas.client.gui;
-
-/** Listener for button select in a RadioGroup. */
-public interface ISelectListener<B extends GuiRadioButton> {
-	/** Called when a button in the group was selected.
-	 * @param button the button which was selected.
-	 */
-	void onSelect(B button);
-}
