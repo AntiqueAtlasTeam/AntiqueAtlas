251ea0e31fd08da108a0375fa299a1d2bf378368
Hunternif <hunternif@gmail.com>
2014-04-29 18:02:21 +0400

    Fixed IllegalArgumentException when overwriting Tile ID.

--- src/main/java/hunternif/mc/atlas/AntiqueAtlasMod.java
+++ src/main/java/hunternif/mc/atlas/AntiqueAtlasMod.java
@@ -30,7 +30,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class AntiqueAtlasMod {
 	public static final String ID = "antiqueatlas";
 	public static final String NAME = "Antique Atlas";
-	public static final String VERSION = "3.1-1.6.4";
+	public static final String VERSION = "3.1.1-1.6.4";
 	public static final String CHANNEL = ID;
 	
 	@Instance(ID)
