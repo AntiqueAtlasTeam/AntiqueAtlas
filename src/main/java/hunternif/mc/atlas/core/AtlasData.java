99202d63eed402b8336a33f5b95f3631b82c3dc5
Christopher Waugh <kenkron@gmail.com>
2017-01-06 19:50:15 -0500

    new nbt format for AtlasData/

--- src/main/java/hunternif/mc/atlas/core/AtlasData.java
+++ src/main/java/hunternif/mc/atlas/core/AtlasData.java
@@ -16,13 +16,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.WorldSavedData;
 import net.minecraftforge.common.util.Constants;
 
 public class AtlasData extends WorldSavedData {
-	public static final int VERSION = 2;
+	public static final int VERSION = 3;
 	public static final String TAG_VERSION = "aaVersion";
 	public static final String TAG_DIMENSION_MAP_LIST = "qDimensionMap";
 	public static final String TAG_DIMENSION_ID = "qDimensionID";
@@ -60,11 +61,9 @@ public class AtlasData extends WorldSavedData {
 		for (int d = 0; d < dimensionMapList.tagCount(); d++) {
 			NBTTagCompound dimTag = dimensionMapList.getCompoundTagAt(d);
 			int dimensionID = dimTag.getInteger(TAG_DIMENSION_ID);
-			int[] intArray = dimTag.getIntArray(TAG_VISITED_CHUNKS);
+			NBTTagList dimensionTag = (NBTTagList) dimTag.getTag(TAG_VISITED_CHUNKS);
 			DimensionData dimData = getDimensionData(dimensionID);
-			for (int i = 0; i < intArray.length; i += 3) {
-				dimData.setTile(intArray[i], intArray[i+1], new Tile(intArray[i+2]));
-			}
+			dimData.readFromNBT(dimensionTag);
 			double zoom = (double)dimTag.getInteger(TAG_BROWSING_ZOOM) / BrowsingPositionPacket.ZOOM_SCALE_FACTOR;
 			if (zoom == 0) zoom = 0.5;
 			dimData.setBrowsingPosition(dimTag.getInteger(TAG_BROWSING_X),
@@ -104,15 +103,15 @@ public class AtlasData extends WorldSavedData {
 			NBTTagCompound dimTag = new NBTTagCompound();
 			dimTag.setInteger(TAG_DIMENSION_ID, dimensionEntry.getKey().intValue());
 			DimensionData dimData = dimensionEntry.getValue();
-			Map<ShortVec2, Tile> seenChunks = dimData.getSeenChunks();
-			int[] intArray = new int[seenChunks.size()*3];
-			int i = 0;
-			for (Entry<ShortVec2, Tile> entry : seenChunks.entrySet()) {
-				intArray[i++] = entry.getKey().x;
-				intArray[i++] = entry.getKey().y;
-				intArray[i++] = entry.getValue().biomeID;
-			}
-			dimTag.setIntArray(TAG_VISITED_CHUNKS, intArray);
+//			Map<ShortVec2, Tile> seenChunks = dimData.getSeenChunks();
+//			int[] intArray = new int[seenChunks.size()*3];
+//			int i = 0;
+//			for (Entry<ShortVec2, Tile> entry : seenChunks.entrySet()) {
+//				intArray[i++] = entry.getKey().x;
+//				intArray[i++] = entry.getKey().y;
+//				intArray[i++] = entry.getValue().biomeID;
+//			}
+			dimTag.setTag(TAG_VISITED_CHUNKS, dimData.writeToNBT());
 			dimTag.setInteger(TAG_BROWSING_X, dimData.getBrowsingX());
 			dimTag.setInteger(TAG_BROWSING_Y, dimData.getBrowsingY());
 			dimTag.setInteger(TAG_BROWSING_ZOOM, (int)Math.round(dimData.getBrowsingZoom() * BrowsingPositionPacket.ZOOM_SCALE_FACTOR));
@@ -177,5 +176,16 @@ public class AtlasData extends WorldSavedData {
 
 	public boolean isEmpty() {
 		return dimensionMap.isEmpty();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof AtlasData)) return false;
+		AtlasData other = (AtlasData) obj;
+		if (other.dimensionMap.size()!=dimensionMap.size()) return false;
+		for (Integer key: dimensionMap.keySet()){
+			if (!dimensionMap.get(key).equals(other.dimensionMap.get(key))) return false;
+		}
+		return true;
 	}
 }
