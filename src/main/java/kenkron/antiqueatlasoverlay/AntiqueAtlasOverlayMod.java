f27ef5b68e2eb4c1600d72a722a157b66f4ce819
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-06-05 15:21:57 -0700

    Properly mark AAO as a client only mod

--- src/main/java/kenkron/antiqueatlasoverlay/AntiqueAtlasOverlayMod.java
+++ src/main/java/kenkron/antiqueatlasoverlay/AntiqueAtlasOverlayMod.java
@@ -1,28 +1,24 @@
 package kenkron.antiqueatlasoverlay;
 
+import hunternif.mc.atlas.AntiqueAtlasMod;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = AntiqueAtlasOverlayMod.MODID, version = AntiqueAtlasOverlayMod.VERSION, name = AntiqueAtlasOverlayMod.MODID, dependencies = "required-after:antiqueatlas", guiFactory = "kenkron.antiqueatlasoverlay.gui.AAOConfigGuiFactory")
+import java.io.File;
+
+@Mod(modid = AntiqueAtlasOverlayMod.MODID, version = AntiqueAtlasOverlayMod.VERSION, name = AntiqueAtlasOverlayMod.MODID, clientSideOnly = true, dependencies = "required-after:antiqueatlas", guiFactory = "kenkron.antiqueatlasoverlay.gui.AAOConfigGuiFactory")
 public class AntiqueAtlasOverlayMod {
     public static final String MODID = "antiqueatlasoverlay";
     public static final String VERSION = "1.2";
 
-    @SidedProxy(clientSide = "kenkron.antiqueatlasoverlay.AAOClient",
-            serverSide = "kenkron.antiqueatlasoverlay.AAOCommon")
-    private static AAOCommon proxy;
-
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        System.out.println(proxy.getClass());
-        proxy.preInit(event);
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        proxy.init(event);
+        AAORenderEventReceiver renderer = new AAORenderEventReceiver();
+        AAOConfig.load(new File(AntiqueAtlasMod.proxy.getConfigDir(), "/AntiqueAtlasOverlay.cfg"), renderer);
+        MinecraftForge.EVENT_BUS.register(renderer);
     }
 }
