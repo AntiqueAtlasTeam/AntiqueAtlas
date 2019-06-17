844ddb3aa430182b2b0f4ae751d0393bcc246eac
tyra314 <tyra_oa@icloud.com>
2019-06-17 22:23:04 +0200

    Fixes on death marker

--- src/main/java/hunternif/mc/atlas/ext/watcher/DeathWatcher.java
+++ src/main/java/hunternif/mc/atlas/ext/watcher/DeathWatcher.java
@@ -2,8 +2,8 @@ package hunternif.mc.atlas.ext.watcher;
 
 import hunternif.mc.atlas.SettingsConfig;
 import hunternif.mc.atlas.api.AtlasAPI;
-import hunternif.mc.atlas.registry.MarkerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.chat.TranslatableComponent;
 
 /**
  * Puts an skull marker to the player's death spot.
@@ -14,7 +14,7 @@ public class DeathWatcher {
 		if (SettingsConfig.gameplay.autoDeathMarker) {
 			for (int atlasID : AtlasAPI.getPlayerAtlases(player)) {
 				AtlasAPI.markers.putMarker(player.getEntityWorld(), true, atlasID, "antiqueatlas:tomb",
-						"gui.antiqueatlas.marker.tomb " + player.getName(),
+						new TranslatableComponent("gui.antiqueatlas.marker.tomb").append(player.getName()).getString(),
 						(int)player.x, (int)player.z);
 			}
 		}
--- src/main/java/hunternif/mc/atlas/mixin/MixinPlayerEntity.java
+++ src/main/java/hunternif/mc/atlas/mixin/MixinPlayerEntity.java
@@ -1,8 +1,6 @@
 package hunternif.mc.atlas.mixin;
 
 import hunternif.mc.atlas.core.PlayerEventHandler;
-import hunternif.mc.atlas.ext.watcher.DeathWatcher;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,10 +9,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public class MixinPlayerEntity {
-    @Inject(at = @At("HEAD"), method = "onDeath")
-    public void onDeath(DamageSource source, CallbackInfo info) {
-        DeathWatcher.onPlayerDeath((PlayerEntity) (Object) this);
-    }
 
     @Inject(at = @At("RETURN"), method = "tick")
     public void tick(CallbackInfo info) {
