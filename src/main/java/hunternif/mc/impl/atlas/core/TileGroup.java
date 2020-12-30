786802f01a4f4fa586a22e601a121cb3073a5a6c
tyra314 <tyra_oa@icloud.com>
2020-12-30 23:55:01 +0100

    Fixes oceans everywhere

--- src/main/java/hunternif/mc/impl/atlas/core/TileGroup.java
+++ src/main/java/hunternif/mc/impl/atlas/core/TileGroup.java
@@ -61,7 +61,7 @@ public class TileGroup implements ITileStorage {
 				// 0 1 2
 				// 3 4 5
 				// 6 7 8
-				listTag.add(x + y * CHUNK_STEP, StringTag.of(this.tiles[x][y] == null ? "minecraft:ocean" : this.tiles[x][y].toString()));
+				listTag.add(x + y * CHUNK_STEP, StringTag.of(this.tiles[x][y] == null ? "antiqueatlas:%null#" : this.tiles[x][y].toString()));
 			}
 		}
 
