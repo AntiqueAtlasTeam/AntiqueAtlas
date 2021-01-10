f85a7c22205fd55d4072515574928551569ac9e5
tyra314 <tyra_oa@icloud.com>
2021-01-10 02:29:41 +0100

    Adds ModMenu integration. Fixes #294

---
+++ src/main/java/hunternif/mc/impl/atlas/client/AntiqueAtlasModMenu.java
@@ -0,0 +1,16 @@
+package hunternif.mc.impl.atlas.client;
+
+import hunternif.mc.impl.atlas.AntiqueAtlasConfig;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class AntiqueAtlasModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(AntiqueAtlasConfig.class, parent).get();
+    }
+}
