53a557867939e46595f410a9d8101f8dc4f9932e
tyra314 <tyra_oa@icloud.com>
2021-01-25 21:47:01 +0100

    Replace key cache with simple allocation.
    The construction of a new key takes only a third of the time, a thread-safe map access costs. And the code is less complicated.

--- src/main/java/hunternif/mc/impl/atlas/marker/DimensionMarkersData.java
+++ src/main/java/hunternif/mc/impl/atlas/marker/DimensionMarkersData.java
@@ -17,41 +17,33 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class DimensionMarkersData {
 	private final MarkersData parent;
 	private final RegistryKey<World> world;
-	
+
 	private int size = 0;
-	
+
 	private final Map<ShortVec2 /*chunk coords*/, List<Marker>> chunkMap =
 			new ConcurrentHashMap<>(2, 0.75f, 2);
-	
+
 	private final Values values = new Values();
-	
-	/** Maps threads to the temporary key for thread-safe access to chunkMap. */
-	private final Map<Thread, ShortVec2> thread2KeyMap = new ConcurrentHashMap<>(2, 0.75f, 2);
-	
-	/** Temporary key for thread-safe access to chunkMap. */
-	private ShortVec2 getKey() {
-		return thread2KeyMap.computeIfAbsent(Thread.currentThread(), k -> new ShortVec2(0, 0));
-	}
-	
+
 	public DimensionMarkersData(MarkersData parent, RegistryKey<World> world) {
 		this.parent = parent;
 		this.world = world;
 	}
-	
+
 	public RegistryKey<World> getWorld() {
 		return world;
 	}
-	
+
 	/** The "chunk" here is {@link MarkersData#CHUNK_STEP} times larger than the
 	 * Minecraft 16x16 chunk! */
 	public List<Marker> getMarkersAtChunk(int x, int z) {
-		return chunkMap.get(getKey().set(x, z));
+		return chunkMap.get(new ShortVec2(x, z));
 	}
-	
+
 	/** Insert marker into a list at chunk coordinates, maintaining the ordering
 	 * of the list by Z coordinate. */
 	public void insertMarker(Marker marker) {
-		ShortVec2 key = getKey().set(
+		ShortVec2 key = new ShortVec2(
 				marker.getChunkX() / MarkersData.CHUNK_STEP,
 				marker.getChunkZ() / MarkersData.CHUNK_STEP);
 		List<Marker> list = chunkMap.get(key);
@@ -73,19 +65,19 @@ public class DimensionMarkersData {
 		size++;
 		parent.markDirty();
 	}
-	
+
 	public boolean removeMarker(Marker marker) {
 		size--;
 		return getMarkersAtChunk(
 				marker.getChunkX() / MarkersData.CHUNK_STEP,
 				marker.getChunkZ() / MarkersData.CHUNK_STEP).remove(marker);
 	}
-	
+
 	/** The returned view is immutable, i.e. remove() won't work. */
 	public Collection<Marker> getAllMarkers() {
 		return values;
 	}
-	
+
 	private class Values extends AbstractCollection<Marker> {
 		@Override
 		public Iterator<Marker> iterator() {
@@ -95,6 +87,6 @@ public class DimensionMarkersData {
 		public int size() {
 			return size;
 		}
-		
+
 	}
 }
