1a239c5ebd97c45cb8b6bd93865d08b2a12fbedd
tyra314 <tyra_oa@icloud.com>
2021-01-28 21:01:29 +0100

    Fixed mipping for end city markers

--- src/main/java/hunternif/mc/impl/atlas/registry/MarkerType.java
+++ src/main/java/hunternif/mc/impl/atlas/registry/MarkerType.java
@@ -155,7 +155,7 @@ public class MarkerType {
 		}
 
 		if (icons.length > 1) {
-			int smallestSide = (int) (size * screenScale);
+			int smallestSide = (int) (size);
 
 			int closestValue = Integer.MAX_VALUE;
 			int closestIndex = -1;
