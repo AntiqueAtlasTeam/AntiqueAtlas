6cef8e74bf0a1b60799a559939c988cbf6733ba6
tyra314 <tyra_oa@icloud.com>
2021-04-02 20:46:11 +0200

    Fixes itemNeeded handling in during registration

--- src/main/java/hunternif/mc/impl/atlas/RegistrarAntiqueAtlas.java
+++ src/main/java/hunternif/mc/impl/atlas/RegistrarAntiqueAtlas.java
@@ -15,19 +15,12 @@ public class RegistrarAntiqueAtlas {
     public static final AtlasItem ATLAS = new AtlasItem(new Item.Settings().maxCount(1));
 
     public static void register() {
-        // TODO FABRIC
-        // if (SettingsConfig.gameplay.itemNeeded) {
-        //}
-        Registry.register(Registry.ITEM, new Identifier("antiqueatlas:empty_antique_atlas"), EMPTY_ATLAS);
-        Registry.register(Registry.ITEM, new Identifier("antiqueatlas:antique_atlas"), ATLAS);
+        if (AntiqueAtlasMod.CONFIG.itemNeeded) {
+            Registry.register(Registry.ITEM, new Identifier("antiqueatlas:empty_antique_atlas"), EMPTY_ATLAS);
+            Registry.register(Registry.ITEM, new Identifier("antiqueatlas:antique_atlas"), ATLAS);
 
-        Registry.register(Registry.RECIPE_SERIALIZER, "antiqueatlas:atlas_clone", RecipeAtlasCloning.SERIALIZER);
-        Registry.register(Registry.RECIPE_SERIALIZER, "antiqueatlas:atlas_combine", RecipeAtlasCombining.SERIALIZER);
-
-        /* if (SettingsConfig.gameplay.itemNeeded) {
-            event.getRegistry().register(new ShapelessOreRecipe(new Identifier(AntiqueAtlasMod.ID, "atlas"), new ItemStack(EMPTY_ATLAS), aud.XX_1_13_2_aS_XX, aud.XX_1_13_2_aX_XX).setRegistryName("atlas_blank"));
-            event.getRegistry().register(new RecipeAtlasCloning().setRegistryName("atlas_clone"));
-            event.getRegistry().register(new RecipeAtlasCombining().setRegistryName("atlas_combine"));
-        } */
+            Registry.register(Registry.RECIPE_SERIALIZER, "antiqueatlas:atlas_clone", RecipeAtlasCloning.SERIALIZER);
+            Registry.register(Registry.RECIPE_SERIALIZER, "antiqueatlas:atlas_combine", RecipeAtlasCombining.SERIALIZER);
+        }
     }
 }
