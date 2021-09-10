67716614e97da5229c3e6631b05dfc85b6f0ed23
tyra314 <tyra_oa@icloud.com>
2021-09-10 22:16:46 +0200

    Turns out, I suck at forge modding KEKW
    Gratefully copy stuff from stereo's forge branch.
    
    Co-authored-by: Stereowalker <stereowalker.mc@gmail.com>

--- forge/src/main/java/hunternif/mc/impl/atlas/AntiqueAtlasModForge.java
+++ forge/src/main/java/hunternif/mc/impl/atlas/AntiqueAtlasModForge.java
@@ -2,36 +2,34 @@ package hunternif.mc.impl.atlas;
 
 import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AntiqueAtlasMod.ID)
 public class AntiqueAtlasModForge
 {
-    public AntiqueAtlasModForge() {
+    public AntiqueAtlasModForge()
+    {
         // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(AntiqueAtlasMod.ID, FMLJavaModLoadingContext.get().getModEventBus());
+        EventBuses.registerModEventBus(AntiqueAtlasMod.ID,
+                FMLJavaModLoadingContext.get().getModEventBus());
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::initializeClient);
+
+        MinecraftForge.EVENT_BUS.register(this);
         AntiqueAtlasMod.init();
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-    }
-
-    public void clientRegistries(final FMLClientSetupEvent event)
-    {
-        AntiqueAtlasModClient.init();
     }
 
     /**
      * Despite what the events name might suggest, this event can be used for more than registering
      * particle renders as it's called in the {@link net.minecraft.client.MinecraftClient} constructor.
      * Thus here we use it to do all client stuff
+     *
      * @param event
      */
-    public void initializeClient(ParticleFactoryRegisterEvent event) {
+    public void initializeClient(ParticleFactoryRegisterEvent event)
+    {
         AntiqueAtlasModClient.init();
     }
 }
