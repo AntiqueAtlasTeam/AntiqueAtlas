3d1604710b2966d35fac14d7db20fd5b034fd496
tyra314 <tyra_oa@icloud.com>
2022-09-13 15:52:00 +0200

    Fixes crash when dragging on a marker in atlas gui

--- common/src/main/java/hunternif/mc/impl/atlas/event/MarkerClickedCallback.java
+++ common/src/main/java/hunternif/mc/impl/atlas/event/MarkerClickedCallback.java
@@ -2,12 +2,13 @@ package hunternif.mc.impl.atlas.event;
 
 import dev.architectury.event.Event;
 import dev.architectury.event.EventFactory;
+import dev.architectury.event.EventResult;
 import hunternif.mc.impl.atlas.marker.Marker;
 import net.minecraft.entity.player.PlayerEntity;
 
 @FunctionalInterface
 public interface MarkerClickedCallback {
-    Event<MarkerClickedCallback> EVENT = EventFactory.createLoop();
+    Event<MarkerClickedCallback> EVENT = EventFactory.createEventResult();
 
-    boolean onClicked(PlayerEntity player, Marker marker, int mouseState);
+    EventResult onClicked(PlayerEntity player, Marker marker, int mouseState);
 }
