c824b9d2bdcb38f7c4a6f625ee2b16635b0fce67
tyra314 <tyra_oa@icloud.com>
2022-09-14 21:50:03 +0200

    Fix KeyBindings acting weird
    Fixes #436

--- common/src/main/java/hunternif/mc/impl/atlas/client/KeyHandler.java
+++ common/src/main/java/hunternif/mc/impl/atlas/client/KeyHandler.java
@@ -1,6 +1,5 @@
 package hunternif.mc.impl.atlas.client;
 
-import dev.architectury.injectables.annotations.ExpectPlatform;
 import hunternif.mc.impl.atlas.AntiqueAtlasModClient;
 import hunternif.mc.impl.atlas.client.gui.GuiAtlas;
 import me.shedaniel.architectury.registry.KeyBindings;
@@ -12,32 +11,16 @@ import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Environment(EnvType.CLIENT)
 public class KeyHandler {
-    /**
-     * ID's of keys
-     */
-    private static final int KEY_ATLAS = 0;
+    public static final KeyBinding ATLAS_KEYMAPPING = new KeyBinding("key.openatlas.desc", InputUtil.Type.KEYSYM, 77, "key.antiqueatlas.category");
 
-    /**
-     * List of bindings (at this moment with only one binding)
-     */
-    private static List<KeyBinding> bindings = new ArrayList<>(1);
-
-    @ExpectPlatform
     public static void registerBindings() {
-        // Initialisation of bindings
-        bindings.add(KEY_ATLAS, new KeyBinding("key.openatlas.desc", InputUtil.Type.KEYSYM, 77, "key.antiqueatlas.category"));
-
-        // Registering all binding
-        bindings.forEach(KeyBindings::registerKeyBinding);
+        KeyBindings.registerKeyBinding(ATLAS_KEYMAPPING);
     }
 
     public static void onClientTick(MinecraftClient client) {
-        if (bindings.get(KEY_ATLAS).wasPressed()) {
+        while (ATLAS_KEYMAPPING.wasPressed()) {
             Screen currentScreen = MinecraftClient.getInstance().currentScreen;
             if (currentScreen instanceof GuiAtlas) {
                 currentScreen.onClose();
